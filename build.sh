6577ddc7ec2f5b499b2a2974409dfa03045e6ae4
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-12-04 17:43:58 +0300

    stash

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-docker build -t zaidulinkirill96/ffmpeg-test:6.12 .
-docker push zaidulinkirill96/ffmpeg-test:6.12
+docker build -t zaidulinkirill96/ffmpeg-test:6.12 && docker push zaidulinkirill96/ffmpeg-test:6.12
+
+
+docker run --platform linux/amd64 zaidulinkirill96/ffmpeg-test:6.12 -i https://ample.revma.ihrhls.com/zc173/23_1n37vikjxofp702/playlist.m3u8 result.mp3
