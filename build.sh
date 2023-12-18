204227048cb12e87aca7582c4803ece7972be3e7
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-12-18 17:11:36 +0300

    stash

--- build.sh
+++ build.sh
@@ -4,10 +4,16 @@ docker build -t zaidulinkirill96/ffmpeg-test:6.12 && docker push zaidulinkirill9
 
 
 # HLS
-docker run --platform linux/amd64 zaidulinkirill96/ffmpeg-test:6.22.4 -re -i https://ample.revma.ihrhls.com/zc173/23_1n37vikjxofp702/playlist.m3u8 -f segment -segment_time 5 chunk-%d.mp3
+docker run --platform linux/amd64 zaidulinkirill96/ffmpeg-test:6.6 -re -i https://live.amperwave.net/manifest/northernstarbroadcasting-wupsfmaac-hlsc2.m3u8?source=v7player -f segment -segment_time 5 -loglevel fatal chunk-%d.mp3
+
+docker run --platform linux/amd64 zaidulinkirill96/ffmpeg-test:6.6 -re -i https://stream.revma.ihrhls.com/zc185/hls.m3u8 -f segment -segment_time 5 -loglevel fatal chunk-%d.mp3
 
 
 # ICY
 # [https @ 0x4002d5e8c0] Metadata update for StreamTitle:  
 # [https @ 0x4002d5e8c0] Metadata update for StreamUrl: 
-docker run --platform linux/amd64 zaidulinkirill96/ffmpeg-test:6.22.4 -re -i https://icecast-qmusicbe-cdp.triple-it.nl/qmusic.mp3?aw_0_1st.disable_pre_roll=true&aw_0_1st.disable_mid_roll=true -f segment -segment_time 5 chunk-%d.mp3
+docker run --platform linux/amd64 zaidulinkirill96/ffmpeg-test:6.6 -re -i https://icecast-qmusicbe-cdp.triple-it.nl/qmusic.mp3?aw_0_1st.disable_pre_roll=true&aw_0_1st.disable_mid_roll=true -f segment -segment_time 5 -loglevel fatal chunk-%d.mp3
+
+
+# Greece
+docker run --platform linux/amd64 zaidulinkirill96/ffmpeg-test:6.6 -re -i https://eco.onestreaming.com:18812/stream -f segment -segment_time 5 -loglevel fatal chunk-%d.mp3
