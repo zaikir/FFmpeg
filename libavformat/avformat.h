b7fcd541cedaff6119d79f4c0908c44ab25db3c1
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-12-22 11:27:38 +0300

    fix

--- libavformat/avformat.h
+++ libavformat/avformat.h
@@ -2641,7 +2641,6 @@ void av_dump_format(AVFormatContext *ic,
 void dump_metadata_test(void *ctx, const AVDictionary *m, const char *indent);
 void set_current_pts_time(int64_t value);
 int64_t get_current_pts_time(int64_t value);
-int64_t current_pts_time;
 
 #define AV_FRAME_FILENAME_FLAGS_MULTIPLE 1 ///< Allow multiple %d
 
